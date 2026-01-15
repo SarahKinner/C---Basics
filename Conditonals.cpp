15a57eb45db3e1ee188fc251d397bf6febe124d5
SarahKinner <sarahkinner0@gmail.com>
2026-01-14 23:13:33 -0700

    Add comments to understand the steps of one of the programs

---
+++ Conditonals.cpp
@@ -0,0 +1,9 @@
+//Even or odd program
+
+//Asks the user to enter in a number
+
+//User inputs a number
+
+//Calculation to figure if the number inputted is even or odd
+
+//If then statement to print depending if it's even or odd
