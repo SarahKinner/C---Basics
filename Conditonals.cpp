18842f1e6f53c5df24f0628620772c427ae0b660
SarahKinner <sarahkinner0@gmail.com>
2026-01-14 23:43:03 -0700

    Added another = for comparsion instead of assignment

--- Conditonals.cpp
+++ Conditonals.cpp
@@ -13,7 +13,7 @@ cin >> x;
 y = x % 2;
 
 //If then statement to print depending if it's even or odd
-if (y = 0) {
+if (y == 0) {
 std::cout << "The number you inputted is a even number." << std::endl;
 } else {
 std::cout << "The number you inputted is a odd number." << std::endl;
