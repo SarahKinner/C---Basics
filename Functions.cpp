1d301ed69074259ea6e3f80c073a19997d09243d
SarahKinner <sarahkinner0@gmail.com>
2026-01-16 00:21:35 -0700

    Tweeking my function

--- Functions.cpp
+++ Functions.cpp
@@ -1,6 +1,10 @@
 //Square of a number
 
 //Make function here where it'll take the input and figures out what the square of the input is
+int squareNumber(int x){
+    calculate result = x * x
+    return result
+}
 
 //Ask user for number
 
