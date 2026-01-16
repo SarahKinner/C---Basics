c4e59e8c3e35d0eedb608f5490f223ef7673e196
SarahKinner <sarahkinner0@gmail.com>
2026-01-16 00:11:02 -0700

    Mmmm yes basic steps needed

--- Functions.cpp
+++ Functions.cpp
@@ -1,1 +1,11 @@
 //Square of a number
+
+//Make function here where it'll take the input and figures out what the square of the input is
+
+//Ask user for number
+
+//User inputs a number
+
+//Call the function
+
+//Print the result
