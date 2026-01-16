c64bf89d58f6a22f4484789a83c487904ad885f4
SarahKinner <sarahkinner0@gmail.com>
2026-01-15 23:53:44 -0700

    Added the first line of the loop

--- Loops.cpp
+++ Loops.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     //Variables that are being stored
     int x;
+    int i;
 
     //Ask user for number
     cout << "Please enter a number: ";
@@ -14,3 +15,6 @@ int main() {
 
 
     //Loop here will go up by one each time until it equals the number the user put
+    for (int i = 1; i <= x; i++){
+
+    }
