eb303332c4eaa8a346f96c8dc5b9dab391025642
SarahKinner <sarahkinner0@gmail.com>
2026-01-15 23:48:51 -0700

    Basically copy and paste code from conditionals to loops with whatever is needed CODING IS FUN

--- Loops.cpp
+++ Loops.cpp
@@ -1,7 +1,16 @@
 //Count from 1 to N
 #include <iostream>
+using namespace std;
 
 int main() {
-//Ask user for number
+    //Variables that are being stored
+    int x;
 
-//Loop here will go up by one each time until it equals the number the user put
+    //Ask user for number
+    cout << "Please enter a number: ";
+
+    //User inputs a number
+    cin >> x;
+
+
+    //Loop here will go up by one each time until it equals the number the user put
