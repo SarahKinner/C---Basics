56a7e423b79848cd305c20fc4c89772763c72e51
SarahKinner <sarahkinner0@gmail.com>
2026-01-19 21:12:58 -0700

    Uhhh added all the steps I'm gonna need to do for the classes program :)

--- Classes.cpp
+++ Classes.cpp
@@ -1,1 +1,25 @@
 //Rectangle Class program
+
+//Create the rectangle class
+
+//Add private section
+
+//Add public section
+
+//Main starts here
+
+//Add the variables needed for the program
+
+//Ask user for width
+
+//User inputs a width
+
+//Ask user for height
+
+//User inputs a height
+
+//Call function
+
+//Print the results
+
+//This will end the program
